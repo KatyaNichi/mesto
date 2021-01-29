6ab3151eb31b52374113d6861b774209bfcd55a0
Ekaterina Nichiporenko <kito-sfera@mail.ru>
2021-01-29 15:30:01 +0100

    fix: after review

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+let popup = document.querySelector('.popup');
+let openBtn = document.querySelector('.profile__edit');
+let closeBtn = document.querySelector('.popup__close-btn');
+let popupInputName = document.querySelector('.popup__input_type_name');
+let popupInputProfession = document.querySelector('.popup__input_type_profession');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    popupInputName.setAttribute('placeholder', profileName.textContent);
+    popupInputProfession.setAttribute('placeholder', profileProfession.textContent);
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    let getNameInput = popupInputName.value;
+    let getJobInput = popupInputProfession.value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+    profileName.textContent = getNameInput;
+    profileProfession.textContent = getJobInput;
+    closePopup()
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+openBtn.addEventListener('click', openPopup);
+
+closeBtn.addEventListener('click', closePopup);
